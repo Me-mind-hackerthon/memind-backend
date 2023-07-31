39342ea1b31c0f3e21d050991352f37864cde346
IngenieurSong <ingenieur.song@gmail.com>
2023-08-01 02:59:34 +0900

    [FEAT] jwt 인증을 이용하도록 router 수정

--- routers/report.py
+++ routers/report.py
@@ -3,25 +3,26 @@ from fastapi import APIRouter, Depends
 from service import ReportHandler
 from schema import ReportInSchema, getReportOutSchema, messageOnlySchema
 from database.connection import get_session
+from auth import authenticate
 
 report_router = APIRouter(
     prefix = "/report", tags = ["report"]
 )
 
 @report_router.get("/{conversation_id}")
-def get_report(user_id, conversation_id, session = Depends(get_session)) -> getReportOutSchema:
-    result = ReportHandler(session, user_id, conversation_id).get_dailyreport()
+def get_report(conversation_id, user: str = Depends(authenticate), session = Depends(get_session)) -> getReportOutSchema:
+    result = ReportHandler(session, conversation_id).get_dailyreport()
 
     return result
 
 @report_router.delete("/{converssation_id}")
-def delete_report(report_input: ReportInSchema, session = Depends(get_session)) -> messageOnlySchema:
-    result = ReportHandler(session, report_input.user_id, report_input.conversation_id).delete_dailyreport()
+def delete_report(report_input: ReportInSchema, user: str = Depends(authenticate), session = Depends(get_session)) -> messageOnlySchema:
+    result = ReportHandler(session, report_input.conversation_id).delete_dailyreport()
 
     return result
 
 @report_router.post("/create-daily")
-def create_report(report_input: ReportInSchema, session = Depends(get_session)) -> messageOnlySchema:
-    result = ReportHandler(session, report_input.user_id, report_input.conversation_id).create_dailyreport()
+def create_report(report_input: ReportInSchema, user: str = Depends(authenticate), session = Depends(get_session)) -> messageOnlySchema:
+    result = ReportHandler(session, report_input.conversation_id).create_dailyreport()
 
     return result
