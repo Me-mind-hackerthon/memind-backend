09cf117996fed9ba1954ef475f804c5f6b065914
IngenieurSong <ingenieur.song@gmail.com>
2023-12-15 18:12:15 +0900

    [FEAT] service/report.py에 srp 원칙과 ocp 원칙을 적용하여 리팩토링 진행

--- service/common.py
+++ service/common.py
@@ -1,3 +1,7 @@
+import os
+import json
+import openai
+
 from abc import ABC, abstractmethod
 
 class ObjectCreator(ABC):
@@ -7,3 +11,21 @@ class ObjectCreator(ABC):
     @abstractmethod
     async def create_object(self, object_info):
         pass
+
+class MessageSender:
+    def __init__(self):
+        openai.api_key(os.environ["GPT_APIKEY"])
+
+    @staticmethod
+    async def send_message(self, chat_data):
+        # OpenAI GPT-3.5 Turbo 모델에 대화를 요청합니다.
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=chat_data
+        )
+
+        # 모델의 답변을 가져옵니다.
+        assistant_response = response['choices'][0]['message']['content']
+        assistant_response = json.loads(assistant_response)
+
+        return assistant_response
