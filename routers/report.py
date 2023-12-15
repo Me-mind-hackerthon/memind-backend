cf45c18ffe4f20f6f5870fe88d56053daa94fb2d
IngenieurSong <ingenieur.song@gmail.com>
2023-12-15 18:14:08 +0900

    [FEAT] #43 report router를 변경된 클래스에 맞춰서 수정하고 비동기로 변경

--- routers/report.py
+++ routers/report.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 
-from service import ReportHandler
-from schema import ReportInSchema, getReportOutSchema, messageOnlySchema,KeywordSchema
+from service import DailyReportCreator, ReportDeletor, ReportLoader
+from schema import ReportInSchema, GetReportOutSchema, MessageOnlySchema, KeywordSchema
 from database.connection import get_session
 from auth import authenticate
 
@@ -9,20 +9,20 @@ report_router = APIRouter(
     prefix = "/api/report", tags = ["report"]
 )
 
-@report_router.get("/{conversation_id}")
-def get_report(conversation_id, user: str = Depends(authenticate), session = Depends(get_session)) -> getReportOutSchema:
-    result = ReportHandler(session, conversation_id).get_dailyreport()
+@report_router.get("/{report_id}")
+async def get_report(report_id, user: str = Depends(authenticate), session = Depends(get_session)) -> GetReportOutSchema:
+    result = await ReportLoader(session).get_report(report_id)
 
     return result
 
-@report_router.delete("/{converssation_id}")
-def delete_report(report_input: ReportInSchema, user: str = Depends(authenticate), session = Depends(get_session)) -> messageOnlySchema:
-    result = ReportHandler(session, report_input.conversation_id).delete_dailyreport()
+@report_router.delete("/{report_id}")
+async def delete_report(report_input: ReportInSchema, user: str = Depends(authenticate), session = Depends(get_session)) -> MessageOnlySchema:
+    result = await ReportDeletor(session).delete_report(report_input.report_id)
 
     return result
 
 @report_router.post("/create-daily")
-def create_report(report_input: ReportInSchema, user: str = Depends(authenticate), session = Depends(get_session)) -> KeywordSchema:
-    result = ReportHandler(session, report_input.conversation_id).create_dailyreport()
+async def create_report(report_input: ReportInSchema, user: str = Depends(authenticate), session = Depends(get_session)) -> KeywordSchema:
+    result = await DailyReportCreator(session).create_dailyreport()
 
     return result
